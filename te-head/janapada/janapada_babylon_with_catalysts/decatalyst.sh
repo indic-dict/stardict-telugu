8cfdaab5ab12bd89896a3372919bc3f6a92be06a
damodar108 <yaminichenna@gmail.com>
2016-03-12 23:10:06 +0530

    devanagari hrasvas added, root words for vibhakti pratyayas added

--- te-head/janapada/janapada_babylon_with_catalysts/decatalyst.sh
+++ te-head/janapada/janapada_babylon_with_catalysts/decatalyst.sh
@@ -1,13 +1,18 @@
-sed -i  '/qqqq/s/#IUP#/#/' ./*.html
-sed -i  '/qqqq/s/#IUI#/#/' ./*.html
-sed -i  '/qqqq/s/#PPP#/#/' ./*.html
-sed -i  '/qqqq/s/#NNN#/#/' ./*.html
-sed -i  '/qqqq/s/#DDD#/#/' ./*.html
-sed -i  '/qqqq/s/#QQ#/#/' ./*.html
-sed -i  '/qqqq/s/xxx#/#/' ./*.html
-sed -i  '/qqqq/s/#@@@//' ./*.html
+sed -i  '/qqqq/s/#VPT#/#/' ./*.babylon
+sed -i  '/qqqq/s/#VPH#/#/' ./*.babylon
+sed -i  '/qqqq/s/#VPE#/#/' ./*.babylon
+sed -i  '/qqqq/s/#DHR#/#/' ./*.babylon
 
-sed -i '/qqqq/s/#/|/g' ./*.html
-sed -i 's/ *qqqq *| *//' ./*.html
-sed -i 's/ *zzzz *//' ./*.html
-sed -i 's/౛/జ/g' ./*.html
+sed -i  '/qqqq/s/#IUP#/#/' ./*.babylon
+sed -i  '/qqqq/s/#IUI#/#/' ./*.babylon
+sed -i  '/qqqq/s/#PPP#/#/' ./*.babylon
+sed -i  '/qqqq/s/#NNN#/#/' ./*.babylon
+sed -i  '/qqqq/s/#DDD#/#/' ./*.babylon
+sed -i  '/qqqq/s/#QQ#/#/' ./*.babylon
+sed -i  '/qqqq/s/xxx#/#/' ./*.babylon
+sed -i  '/qqqq/s/#@@@//' ./*.babylon
+
+sed -i '/qqqq/s/#/|/g' ./*.babylon
+sed -i 's/ *qqqq *| *//' ./*.babylon
+sed -i 's/ *zzzz *//' ./*.babylon
+sed -i 's/౛/జ/g' ./*.babylon
